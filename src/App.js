ccb644ea105483b6b07d1ff1a7cd78652f524b4b
maxuel99 <massimocaser@gmail.com>
2022-08-22 19:37:50 +0200

    add components-06

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export class App extends React.Component {
         return (
             <div>
                 <HelloWorld />
+                <p>What a beautiful day!</p>
             </div>
         )
     }
