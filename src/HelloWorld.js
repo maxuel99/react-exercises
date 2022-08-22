94d3b6a16db583a5a528d0cd8788148936920a52
maxuel99 <massimocaser@gmail.com>
2022-08-22 19:48:33 +0200

    add components-07

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Message } from './Message'
 
 export class HelloWorld extends React.Component {
     render() {
-        return <h1>Hello, World!</h1> 
+        return (
+            <div>
+                <h1>Hello, World!</h1> 
+                <Message />
+            </div>
+        ) 
     }
 }
