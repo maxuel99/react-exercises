6ae6faea0ac338e7083617f226e314d67c931e14
maxuel99 <massimocaser@gmail.com>
2022-08-22 21:21:26 +0200

    add props-01

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { HelloWorld } from "./HelloWorld";
+import { Welcome } from "./Welcome";
 
 export class App extends React.Component {
     render() {
-        return <HelloWorld />
+        return (
+            <div>
+                <HelloWorld />
+                <Welcome name="Massimo" />
+            </div>
+        )
     }
 }
