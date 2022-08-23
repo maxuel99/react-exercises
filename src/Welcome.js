f781d6ba8612ecb1ef1d0c3c3fce330ba86b0559
maxuel99 <massimocaser@gmail.com>
2022-08-23 15:29:26 +0200

    add props-04

--- src/Welcome.js
+++ src/Welcome.js
@@ -2,9 +2,10 @@ import React from "react";
 
 export class Welcome extends React.Component {
     render() {
+        const name = 'Massimo'
         return (
             <div>
-                <p>Welcome, {this.props.name}</p>
+                <p>Welcome, <strong>{name}</strong></p>
                 <p>Your age is, {this.props.age}</p>
             </div>
         )
