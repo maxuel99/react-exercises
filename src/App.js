cabe86e1e546735fb8a680a0989e7004b5e6a06a
maxuel99 <massimocaser@gmail.com>
2022-08-24 13:12:02 +0200

    add state-01

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Counter } from "./Counter";
 import { HelloWorld } from "./HelloWorld";
 import { Welcome } from "./Welcome";
 
@@ -8,6 +9,7 @@ export class App extends React.Component {
             <div>
                 <HelloWorld />
                 <Welcome age='23' />
+                <Counter />
             </div>
         )
     }
