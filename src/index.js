a29ab1751c613d8b358079d3bc271f9f135cf584
maxuel99 <massimocaser@gmail.com>
2022-09-01 14:43:14 +0200

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import ReactDOM from 'react-dom'
 import { App } from './App'
 import './index.css'
-// import '../node_modules/react-bootstrap/dist/react-bootstrap' //script react-bootstap
-import '../node_modules/bootstrap/dist/css/bootstrap.css'        //css react-boostrap
+
+/* import '../node_modules/react-bootstrap/dist/react-bootstrap' //script react-bootstap*/
+import '../node_modules/bootstrap/dist/css/bootstrap.css'        /*css react-boostrap*/
 
 const root = document.querySelector('#root')
 
