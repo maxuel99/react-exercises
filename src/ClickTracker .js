b5c1bdbc20f4b54204965e6808c55de571c57c22
maxuel99 <massimocaser@gmail.com>
2022-09-01 11:57:01 +0200

    add fix

--- src/ClickTracker .js
+++ src/ClickTracker .js
@@ -1,16 +1,27 @@
 import React from "react";
+import { Button } from 'react-bootstrap';
 
 export class CLickTracker extends React.Component {
+    state = {
+        track: ''
+    }
+
     handleButton = (event) => {
-        console.log(event.timeStamp)
+        console.log(event.target.innerText)
+        this.setState(() => {
+            return {
+                track: event.target.innerText
+            }
+        })
     }
     
     render() {
         return (
-            <div onClick={this.handleButton} className="mx-3 mt-2">
-                <button><img src="https://cdn-icons-png.flaticon.com/512/7645/7645064.png" alt=""  width={'50px'}/></button>
-                <button><img src="https://cdn-icons-png.flaticon.com/512/7645/7645252.png" alt=""  width={'50px'}/></button>
-                <button><img src="https://cdn-icons-png.flaticon.com/512/7645/7645255.png" alt=""  width={'50px'}/></button>
+            <div className="mx-3 mt-2">
+                <Button className="border mr-1" name="my-btn" onClick={this.handleButton} >Primo</Button>
+                <Button variant="secondary" className="border mr-1" name="my-btn-2" onClick={this.handleButton} >Secondo</Button>
+                <Button variant="success" className="border" name="my-btn-3" onClick={this.handleButton} >Terzo</Button>
+                <h1>{this.state.track}</h1>
             </div>
         )
     }
