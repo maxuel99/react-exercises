0b4e96e939367ae995d3d39a58a5faf964c2732b
maxuel99 <massimocaser@gmail.com>
2022-09-05 00:58:35 +0200

    add function-Components-03

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ export class App extends React.Component {
                             )
                         }}
                     </TodoList>
-                    <Sum number={[2, 4, 6, 8]}/>
+                    <Sum />
                 </Container>  
         )
     }
--- src/Sum.js
+++ src/Sum.js
@@ -1,10 +1,17 @@
 export function Sum(props) {
     
-    const sum = props.number.reduce((accumulator, value) => {
+    const sum = props.numbers.reduce((accumulator, value) => {
         return accumulator + value;
     },0)
 
     return (
         <h1 className="ml-4">{sum}</h1>
     )
+}
+
+//What happens if the numbers prop of the Sum component is not set? The value of sum is zero
+//How can you set a default value for this prop?
+
+Sum.defaultProps = {
+    numbers: [2, 5, 3],
 }
