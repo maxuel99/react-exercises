3ce254a7f98500b61429270b61c412e59e3cf0cc
maxuel99 <massimocaser@gmail.com>
2022-09-06 19:11:47 +0200

    add h1 instead h4

--- src/Sum.js
+++ src/Sum.js
@@ -7,7 +7,7 @@ export function Sum(props) {
     return (
         <div>
             <h2 className="text-2xl font-bold underline mx-3 mb-2">The sum of "{props.numbers}" is:</h2>
-            <h4 className="ml-4">{sum}</h4>
+            <h1 className="ml-4">{sum}</h1>
         </div>
     )
 }
