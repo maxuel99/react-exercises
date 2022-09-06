7058feb60e9eb331e3cfdc62e217f746eb259ced
maxuel99 <massimocaser@gmail.com>
2022-09-06 14:22:07 +0200

    fix error console log

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import React from 'react'
 import './index.css'
 
 /* import '../node_modules/react-bootstrap/dist/react-bootstrap' //script react-bootstap*/
 import '../node_modules/bootstrap/dist/css/bootstrap.css'        /*css react-boostrap*/
 
-const root = document.querySelector('#root')
+const root = ReactDOM.createRoot(document.querySelector('#root'))
 
-ReactDOM.render(<App />, root)
+root.render(<App />)
