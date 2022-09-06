c7fd389ff6f2399c23b4fbb9a162759ab0ba6af9
maxuel99 <massimocaser@gmail.com>
2022-09-06 19:10:19 +0200

    add style

--- src/Sum.js
+++ src/Sum.js
@@ -5,6 +5,9 @@ export function Sum(props) {
     },0)
 
     return (
-        <h1 className="ml-4">{sum}</h1>
+        <div>
+            <h2 className="text-2xl font-bold underline mx-3 mb-2">The sum of "{props.numbers}" is:</h2>
+            <h4 className="ml-4">{sum}</h4>
+        </div>
     )
 }
