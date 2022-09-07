4de8d1958c8012d23efbe7b8184d0321c3c19016
maxuel99 <massimocaser@gmail.com>
2022-09-07 17:59:06 +0200

    add custom-hooks-04

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,12 +1,14 @@
 import { useGithubUser } from "./useGithubUser";
 
 const GithubUser = ({ username }) => {
-    const { data } = useGithubUser(username)
+    const { data, loading, error } = useGithubUser(username)
 
     return ( 
         <div className="my-3">
             <h2 className="text-2xl font-bold underline mx-3 mb-2">Github Info</h2>
             <ul className="list-inside list-disc">
+                {loading && <h4>Loading...</h4>}
+                {error && <h4>There has been an error</h4>}
                 {data && <li>{data.name} [name]</li>}
                 {data && <li>{data.created_at} [created at]</li>}
                 {data && <li>{data.bio} [bio]</li>}
--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -3,18 +3,29 @@ import { useState } from "react";
 
 export function useGithubUser(username) {
     const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
+        setLoading(true)
         fetch(`https://api.github.com/users/${username}`)
             .then(response => {
+                if (response.status !== 200) {
+                    setError(new Error("User not found"))
+                }
+
                 return response.json()
             })
             .then(json => {
                 // console.log(json)
 
+                setLoading(false)
                 setData(json)
+            })
+            .catch(error => {
+                setError(error)
             })
     }, [username])
 
-    return { data }
+    return { data, loading, error }
 }
